41243125bc8f5b0755669bb7870877b60b93c01a
Himanshu Bhardwaz <himanshu76200@gmail.com>
2021-11-26 00:18:36 +0530

    http 1.0 checks

---
+++ http_one_0.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+
+echo "Enter file name:"
+read filename
+
+declare http_checks=('RewriteEngine On' 'RewriteCond %{THE_REQUEST} !HTTP/1.1$' 'RewriteRule .* - [F]')
+
+if [[ -f $filename ]]
+    then
+        for ((i=0;i<${#http_checks[@]};i++))
+            do
+                if grep -q "${http_checks[i]}" $filename
+                    then
+                        echo "Passed check ${i}"
+                    else
+                        echo "Check ${i} failed..."
+                        echo "Adding required directives to file..."
+                        printf "\n"
+                        echo ${http_checks[i]} >> $filename
+                fi
+            done
+    
+        
+
+else
+    echo "No file exits with this name"
+fi
+    
+
+
+
