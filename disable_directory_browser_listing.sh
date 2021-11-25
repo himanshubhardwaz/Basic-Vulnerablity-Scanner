88bdd12ca255e757b435ab771defc2947fd3c4ff
Himanshu Bhardwaz <himanshu76200@gmail.com>
2021-11-25 23:17:24 +0530

    disable directory browser listing

---
+++ disable_directory_browser_listing.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+echo "Enter file name: "
+read filename
+
+if [[ -f $filename ]]
+    then
+#        sed -i '' 's|<Directory>\([0-9]\{0,\}\)</Directory>|<Directory> '"Options -Indexes"' </Directory>|g' $filename
+
+        sed -i -e '/<Directory>/,/<\/Directory>/d' $filename
+        printf "<Directory> \n Options -Indexes \n </Directory>" >> $filename
+
+#        sed -i '' 's|<Directory>/,/</Directory>/c\<Directory>\nOptions -Indexes \n</Directory>/g' $filename
+
+else
+        echo "File dosen't exits"
+fi
