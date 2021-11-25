be15e8bd4cd4a7fd808476addae8ce43cd801dee
Himanshu Bhardwaz <himanshu76200@gmail.com>
2021-11-25 23:41:59 +0530

    xss protection

---
+++ xss_protection.sh
@@ -0,0 +1,23 @@
+
+#! /bin/bash
+
+echo "Enter file name:"
+read filename
+
+if [[ -f $filename ]]
+    then
+        if grep -q 'Header set X-XSS-Protection "1; mode=block"' $filename
+            then
+                echo "XSS Protection test Passed..."
+        else
+                echo "XSS Protection test Failed..."
+                echo "Adding required directives to file..."
+                printf '\nHeader set X-XSS-Protection "1; mode=block"' >> $filename
+        fi
+
+else
+    echo "No file exits with this name"
+fi
+    
+
+
